849fcab098b0ed7d2cd62270883c424ffefced87
Eleni Oikonomou <oikonomou.g.eleni@outlook.com>
2024-08-13 16:11:53 -0700

    final_prjct

--- components/Sidebar.jsx
+++ components/Sidebar.jsx
@@ -13,8 +13,8 @@ export default function Sidebar(props) {
                 <h4 className="text-snippet">{note.body.split("\n")[0]}</h4>
                 <button 
                     className="delete-btn"
-                    onClick={() => props.setCurrentNoteId(note.id)}
-                >
+                    onClick={() => props.deleteNote(note.id)}
+                    >
                     <i className="gg-trash trash-icon"></i>
                 </button>
             </div>
